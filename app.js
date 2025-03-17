3f3eda141aee0b50bd105620c60d297c69e50f97
Elchristopher <christopherduenas23@cbtis52.edu.mx>
2025-03-16 18:03:11 -0600

    explicacion de el codigo de java script parte 2 (final epico)

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ iconoMenu.addEventListener('click', () => { /*cuando haces click ejecuta la func
 /*Hace que se desplaze dentro o fuera de la pantalla*/
 
     document.body.classList.toggle('opacity');
+    /*Sirve para hacer que se opaque el fondo cuando presionamos el menu hambueguesa */
 
     // Cambiar el ícono
-    const icon = iconoMenu.querySelector('i');
-    if (icon.classList.contains('bi-list')) {
-        icon.classList.remove('bi-list');
-        icon.classList.add('bi-x');
-    } else {
-        icon.classList.remove('bi-x');
-        icon.classList.add('bi-list');
+    const icon = iconoMenu.querySelector('i'); /* selecciona el <i> del HTML*/
+    
+    if (icon.classList.contains('bi-list')) { //Verifica si el ícono tiene la clase 'bi-list'(el ícono de hamburguesa)
+        icon.classList.remove('bi-list'); // si tiene el icono de hamburguesa, se remueve 
+        icon.classList.add('bi-x'); // y añade el icono de equis 
+    } else { //si no se cumple con lo anterior...
+        icon.classList.remove('bi-x'); // se remueve la equis
+        icon.classList.add('bi-list'); // y se anade el icono de hamburguesa
     }
 });
