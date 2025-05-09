4ab957d61fb4797ec15dad89680cd00a57ae0a1d
RoverWuwa <everardoespinoza23@cbtis52.edu.mx>
2025-05-09 17:34:39 -0600

    -Diseño atractivo del boton para la traduccion accesible

--- traducciones/traducciones.js
+++ traducciones/traducciones.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnTraducir = document.getElementById('traducir');
+    const icono = document.getElementById('iconoBtn');
     let currentLang = 'es'; // Idioma inicial
 
     // Cargar traducciones
@@ -10,6 +11,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Cambiar idioma
                 currentLang = currentLang === 'es' ? 'en' : 'es';
                 aplicarTraducciones(translations[currentLang]);
+
+                // Cambiar imagen del botón
+                if (icono.src.includes('usa.png')) {
+                    icono.src = '../img/mexico.png'; // Cambia la imagen al español
+                    icono.alt = 'Icono2'; // Cambia el texto alternativo al español
+                } else {
+                    icono.src = '../img/usa.png'; // Cambia la imagen al inglés
+                    icono.alt = 'Icono1'; // Cambia el texto alternativo al inglés
+                }
             });
         })
         .catch(error => console.error('Error cargando traducciones:', error));
