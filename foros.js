55c02db9072a4e9315e7504cc08f745d63a6dcac
Elchristopher <christopherduenas23@cbtis52.edu.mx>
2025-06-23 01:45:43 -0600

    el chat de los foros fue añadido

--- foros.js
+++ foros.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("DOMContentLoaded", () => {
     
     function createFakeMessage() {
   const mensajeFalso = document.createElement("div");
